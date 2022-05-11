5d6bb0a0a8214e1b51ff7d12985f4f08964215c2
rutecyt <93284291+rutecyt@users.noreply.github.com>
2022-05-11 11:52:46 -0700

    Create initial base repo w/ contract and front-end setup

---
+++ scripts/citation-script.js
@@ -0,0 +1,17 @@
+const hre = require('hardhat');
+
+async function main() {
+  const CitationIPFS = await hre.ethers.getContractFactory('CitationIPS');
+  const citationIPFS = await CitationIPFS.deploy();
+
+  await citationIPFS.deployed();
+
+  console.log('CitationIPFS deployed to:', citationIPFS.address);
+}
+
+main()
+  .then(() => process.exit(0))
+  .catch(error => {
+    console.error(error);
+    process.exit(1);
+  });
